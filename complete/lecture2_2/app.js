f4d52b1916d2e410d58858864186d1fc92155afe
Anginio888 <70545631+Anginio888@users.noreply.github.com>
2021-11-08 05:50:20 -0600

    Update app.js
    back to black

--- complete/lecture2_2/app.js
+++ complete/lecture2_2/app.js
@@ -46,8 +46,6 @@ class App{
     
 	render( ) {   
         this.mesh.rotateY( 0.01 );
-		this.mesh.rotateX( 0.01 );
-		 this.light.rotateY( 0.01 );
         this.renderer.render( this.scene, this.camera );
     }
 }
