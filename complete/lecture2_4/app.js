bbd6547945085372e28bc01146814852be012850
Anginio888 <70545631+Anginio888@users.noreply.github.com>
2021-11-22 15:50:30 -0600

    Update app.js

--- complete/lecture2_4/app.js
+++ complete/lecture2_4/app.js
@@ -1,18 +1,28 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import * as THREE from '../../libs/three125/three.module.js';
+import { GLTFLoader } from '../../libs/three125/GLTFLoader.js';
+import { DRACOLoader } from '../../libs/three125/DRACOLoader.js';
+import { RGBELoader } from '../../libs/three125/RGBELoader.js';
+import { ARButton } from '../../libs/ARButton.js';
+import { LoadingBar } from '../../libs/LoadingBar.js';
 
 class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
         
-		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
-		this.camera.position.set( 0, 0, 4 );
+        this.clock = new THREE.Clock();
+        
+        this.loadingBar = new LoadingBar();
+
+		this.assetsPath = '../../assets/';
+        
+		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+		this.camera.position.set( 0, 0, 0 );
         
 		this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0xaaaaaa );
 
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 2);
+        ambient.position.set( 0.5, 1, 0.25 );
 		this.scene.add(ambient);
         
         const light = new THREE.DirectionalLight();
@@ -22,66 +32,124 @@ class App{
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
 		container.appendChild( this.renderer.domElement );
-		
-        //Replace Box with Circle, Cone, Cylinder, Dodecahedron, Icosahedron, Octahedron, Plane, Sphere, Tetrahedron, Torus or TorusKnot
-
-		const shape = new THREE.Shape();
-		const outerRadius = 1;
-		const innerRadius = .5;
-		const PI2 =Math.PI*3; 
-		const inc = PI2/77;
-		
-		
-		
-		shape.moveTo (outerRadius,0);
-		let inner = true;
-		
-		
-		
-		for (let theta = inc ; theta<PI2; theta+= inc){
-		
-		const radius = (inner ) ? innerRadius : outerRadius;
-			
-			shape.lineTo(Math.cos(theta)*radius, Math.sin(theta)*radius);
-			inner = !inner;
-			
-		
-		}
-		
-		const extrudeSettings = {
-		steps : 1,
-			depth :1,
-			bevelEnabled :false
-			
-			
-		
-		
-		}
-		
-		const geometry = new THREE.ExtrudeGeometry (shape, extruddeSettings);
-		
-		const material = new THREE.MeshPhongMaterial( { color: 0xFF0000 ,specular :0x444444 ,shininess :60 });
-
-        this.mesh = new THREE.Mesh( geometry, material );
+        this.setEnvironment();
         
-        this.scene.add(this.mesh);
+        this.workingVec3 = new THREE.Vector3();
         
-        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+        this.initScene();
+        this.setupXR();
+		
+		window.addEventListener('resize', this.resize.bind(this));
         
-        this.renderer.setAnimationLoop(this.render.bind(this));
+	}
     
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
+    setEnvironment(){
+        const loader = new RGBELoader().setDataType( THREE.UnsignedByteType );
+        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+        pmremGenerator.compileEquirectangularShader();
+        
+        const self = this;
+        
+        loader.load( '../../assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
+          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+          pmremGenerator.dispose();
+
+          self.scene.environment = envMap;
+
+        }, undefined, (err)=>{
+            console.error( 'An error occurred setting the environment');
+        } );
+    }
+	
+    resize(){ 
         this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    	this.camera.updateProjectionMatrix();
+    	this.renderer.setSize( window.innerWidth, window.innerHeight );  
     }
     
-	render( ) {   
-        this.mesh.rotateY( 0.01 );
+    loadCamera(){
+	    const loader = new GLTFLoader().setPath(this.assetsPath);
+        const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderPath( '../../libs/three125/draco/' );
+        loader.setDRACOLoader( dracoLoader );
+		const self = this;
+		
+        // Load a GLTF resource
+		loader.load(
+			// resource URL
+			`steampunk_camera.glb`,
+			// called when the resource is loaded
+			function ( gltf ) {
+				self.model = gltf.scene;
+                self.model.position.set( 0, 0, -1 );
+				self.scene.add( self.model );
+				
+                self.lense = self.model.getObjectByName( "LenseMag" );
+                self.lense.userData.startQuat = self.lense.quaternion.clone();
+                
+                const mixer = new THREE.AnimationMixer( self.model );
+                const action = mixer.clipAction( gltf.animations[0] );
+                action.loop = THREE.LoopOnce;
+                self.action = action;
+                
+                self.mixers.push( mixer );
+                
+                self.loadingBar.visible = false;
+                self.renderer.setAnimationLoop( self.render.bind(self) );
+			},
+			// called while loading is progressing
+			function ( xhr ) {
+
+				self.loadingBar.progress = (xhr.loaded / xhr.total);
+
+			},
+			// called when loading has errors
+			function ( error ) {
+
+				console.error( error.message );
+
+			}
+		);
+	}		
+    
+    initScene(){
+        this.mixers = [];
+        this.collisionObjects = [];
+        this.loadCamera();
+    }
+    
+    setupXR(){
+        this.renderer.xr.enabled = true;
+        
+        const btn = new ARButton( this.renderer, { sessionInit: { optionalFeatures: [ 'dom-overlay' ], domOverlay: { root: document.body } } } );
+        
+        const self = this;
+        
+        function onSelect() {
+            if ( !self.action.isRunning() ){
+                self.action.time = 0;
+                self.action.enabled = true;
+                self.action.play();
+            }
+        }
+
+        this.controller = this.renderer.xr.getController( 0 );
+        this.controller.addEventListener( 'select', onSelect );
+        
+        this.scene.add( this.controller );    
+    }
+    
+
+    render( timestamp, frame ) {
+        const dt = this.clock.getDelta();
+        
+        const self = this;
+        if ( !this.renderer.xr.isPresenting) this.model.rotateY( 0.01 );
+        
+        this.mixers.forEach( mixer => mixer.update(dt) );
+        
         this.renderer.render( this.scene, this.camera );
     }
 }
