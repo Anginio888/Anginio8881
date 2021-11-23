db1389031092588e62a8cf50624f3a33b789e4d2
Anginio888 <70545631+Anginio888@users.noreply.github.com>
2021-11-22 18:30:08 -0600

    Update app.js

--- complete/lecture3_9/app.js
+++ complete/lecture3_9/app.js
@@ -64,9 +64,9 @@ class App{
         function onSelect() {
 		
 		material = new THREE.MeshBasicMaterial( {  } );
-            const material = new THREE.MeshPhongMaterial( {normalMap : texture, emissiveIntensity:2 , displacementMap:texture ,shininess:70} );
+            const material = new THREE.MeshPhongMaterial( {normalMap : texture, displacementScale :1*Math.random(), displacementMap:texture ,shininess:70} );
             const mesh = new THREE.Mesh( self.geometry, material );
-            mesh.position.set( 0, 0, - 0.3* Math.random() ).applyMatrix4( controller.matrixWorld );
+            mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
             mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
             self.scene.add( mesh );
             self.meshes.push( mesh );
